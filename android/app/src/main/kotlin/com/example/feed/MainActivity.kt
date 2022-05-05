06d66c2d1eb7fee8d390d7b928304369ea9c2065
Matula José Alberto <matulajose@hotmail.com>
2022-05-05 07:59:05 +0100

    First
    Start

---
+++ android/app/src/main/kotlin/com/example/feed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.feed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/post/post_informations.dart
@@ -0,0 +1,74 @@
+class Post {
+  static List users = [
+    {
+      "user": {
+        "name": "John Doe",
+        "picture": "assets/images/1_user.jpg",
+      }
+    },
+    {
+      "user": {
+        "name": "Alex Albert",
+        "picture": "assets/images/2_user.jpg",
+      }
+    },
+    {
+      "user": {
+        "name": "Angelina Doe",
+        "picture": "assets/images/3_user.jpg",
+      }
+    },
+    {
+      "user": {
+        "name": "Marcel Marcus",
+        "picture": "assets/images/4_user.jpg",
+      }
+    },
+    {
+      "user": {"name": "Michaela Reis", "picture": "assets/images/5_user.jpg"}
+    }
+  ];
+
+  static List post = [
+    {
+      "post": {
+        "text":
+            "A noite é linda e nada é mais bela do que apreciar ela na melhor compania de sempre, ame alguém e seja feliz ao lado dessa pessoa, resumindo, viva o melhor da vida!",
+        "date": "Thu. Apr 21 17:36",
+        "picture": "assets/images/1_post.jpg",
+      }
+    },
+    {
+      "post": {
+        "text":
+            "As vezes você vai sentir uma vontade enorme de desaparece ou ir para um lugar onde ninguém conhece você, a procura de paz que você sente não ter no seu habitar, a vida tem dessas.",
+        "date": "Mon. Mar 11 10:06",
+        "picture": "assets/images/2_post.jpg",
+      }
+    },
+    {
+      "post": {
+        "text":
+            "Com o avanço da tecnologia, o mundo parece ser pequeno, aliás, ela parece estar nas nossas mãos de certa forma pois com pequenos despositivos nós viajamos o mundo sem descolação física, eu quero ver até onde vamos!",
+        "date": "Sat. Jan 01 07:36",
+        "picture": "assets/images/3_post.jpg",
+      }
+    },
+    {
+      "post": {
+        "text":
+            "Sai do teu lugar de conforto, o mundo tem mais para oferecer, sai da depressão, vai lá fora viver a vida, vai lá e fale com um desconhecido, faça novas amizades, não conhecemos o que virá amanhã mais hoje estamos aqui e precisamos aproveitar ao máximo",
+        "date": "Wed. Apr 12 10:46",
+        "picture": "assets/images/4_post.jpg",
+      }
+    },
+    {
+      "post": {
+        "text":
+            "Se você não fizer nada, nenhum resultado será alcançado, nenhum sonho teu será realizado, você nunca vai comprar aquele carro, você nunca vai construir ou comprar aquela casa, aquela tua ideia que vale milhões nunca vai sair da tua cabeça e se tornar real até o ponto de fazer o mundo olhar para você de uma maneira diferente.",
+        "date": "Sun. Dec 25 20:10",
+        "picture": "assets/images/5_post.jpg",
+      }
+    },
+  ];
+}
